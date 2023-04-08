f4911517140e669ae7aa78e767fbb5ba2720d930
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-04-08 14:44:01 -0300

    Criação do Formulario para adicionar task

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,36 +1,24 @@
 <script  lang="ts">
+import AddFormTask from '../components/FormAddTask.vue'
 export default {
 
+  data() {
+    return {}
+  },
+  components: { AddFormTask },
+  methods: {}
 }
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">Adicionar tarefa</h1>
+  <div class="form-component">
+    <AddFormTask></AddFormTask>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: left;
-}
-
-@media (min-width: 1024px) {
-
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.form-component {
+  width: 100%;
+  align-items: start;
 }
 </style>
---
+++ src/components/FormAddTask.vue
@@ -0,0 +1,51 @@
+<script  lang="ts">
+import type { Task } from '@/core/models/task';
+
+export default {
+
+    data() {
+        return {
+            valid: true,
+            task: {
+                title: "",
+                action: "",
+                done: false
+            } as Task
+        }
+    },
+    methods: {
+        submitForm() {
+            // Aqui você pode enviar os dados do formulário para um servidor
+            console.log({
+                name: this.task.title,
+                email: this.task.action,
+                message: this.task.done,
+            });
+        },
+    }
+}
+</script>
+
+<template>
+    <v-form v-model="valid" class="custom-input">
+        <v-text-field v-model="task.title" label="Titúlo" required ></v-text-field>
+        <v-text-field v-model="task.action" label="Descrição" required ></v-text-field>
+        <v-btn :disabled="!valid" color="primary" class="custom-button" @click="submitForm">Enviar</v-btn>
+    </v-form>
+</template>
+
+<style scoped>
+.custom-input {
+  border-radius: 8px;
+  border: 2px solid #ccc;
+  padding: 10px;
+  margin-bottom: 20px;
+  width: 100%;
+}
+
+.custom-button {
+  border-radius: 8px;
+  padding: 10px;
+  margin-top: 20px;
+}
+</style>
