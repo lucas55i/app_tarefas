f75df4c724d85c1febefc31ea3b6a194830e6b75
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-04-21 18:03:22 -0300

    Reoganizando o componente de task

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts">
-import Tasks from '../components/tasks.vue'
+<script lang="ts">
+import { taskStore } from '@/store/tasks';
+import TaskComponent from '../components/TaskComponents.vue'
+
+export default {
+  components: { TaskComponent },
+  setup() {
+    const userStore = taskStore()
+    return {
+      userStore
+    }
+  },
+  methods: {
+    getTaskCompleted() {
+      return this.userStore.tasks.filter((ta) => ta.done === false).map((p) => p.done)
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
-    <Tasks />
-  </main>
+  <span>
+    <TaskComponent :tasks="userStore.tasks" />
+  </span>
+  <span>
+    <TaskComponent :tasks="userStore.tasks" />
+  </span>
 </template>
