68000bf18fc9c83937c086ade540d0dc6ae9207a
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-04-21 22:28:16 -0300

    Reformulação da organização dos componentes.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,36 @@
 <script lang="ts">
 import { taskStore } from '@/store/tasks';
 import TaskComponent from '../components/TaskComponents.vue'
-import type { Task } from '@/core/models/task';
+import AddTask from '../components/AddTask.vue'
+
 
 export default {
-  components: { TaskComponent },
+  components: { TaskComponent, AddTask },
   setup() {
     const userStore = taskStore()
+    async function getAllTasks() {
+      await userStore.getAll()
+    }
     return {
-      userStore
+      userStore,
+      getAllTasks
     }
   },
   data() {
     return {
     }
   },
-  methods: {},
+  created() {
+    this.getAllTasks()
+  },
 }
 </script>
 
 <template>
+  <header>
+    <AddTask @taskAded="getAllTasks()"></AddTask>
+  </header>
   <span>
-    <TaskComponent :tasks="userStore.tasks" />
+    <TaskComponent :tasks="userStore.tasks" @taskUpdated="getAllTasks()" />
   </span>
 </template>
